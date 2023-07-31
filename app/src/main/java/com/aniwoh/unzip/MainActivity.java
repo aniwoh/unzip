98ef70d617f2350824430c77cd7410712311832e
aniwoh <104836158+aniwoh@users.noreply.github.com>
2023-07-31 21:55:31 +0800

    0

--- app/src/main/java/com/aniwoh/unzip/MainActivity.java
+++ app/src/main/java/com/aniwoh/unzip/MainActivity.java
@@ -1,30 +1,22 @@
 package com.aniwoh.unzip;
 
-import androidx.activity.result.ActivityResult;
-import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.documentfile.provider.DocumentFile;
 
 import android.annotation.SuppressLint;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.DocumentsContract;
 import android.provider.MediaStore;
 import android.provider.Settings;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.io.File;
 import java.io.IOException;
@@ -32,7 +24,6 @@ import java.io.IOException;
 public class MainActivity extends AppCompatActivity {
     private ActivityResultLauncher<Intent> manageExternalStorageLauncher;
     private ActivityResultLauncher<String> filePickerLauncher;
-//    private Button selectfile;
     private TextView pathview;
     private TextView logview;
 
@@ -43,51 +34,37 @@ public class MainActivity extends AppCompatActivity {
 
         pathview=findViewById(R.id.path);
         logview=findViewById(R.id.log);
+
         manageExternalStorageLauncher = registerForActivityResult(
                 new ActivityResultContracts.StartActivityForResult(),
-                new ActivityResultCallback<ActivityResult>() {
-                    @Override
-                    public void onActivityResult(ActivityResult result) {
-                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-                            if (Environment.isExternalStorageManager()) {
-                                // Permission granted
-                                // You can now proceed with your file management operations
-                            } else {
-                                // Permission denied
-                                // Handle the scenario where the user did not grant the permission
-                            }
-                        }
-                    }
+                result -> {
+                    // 下列代码是回调函数
                 });
+        // 调用ActivityResultLauncher必须在初始化后才可以调用
         requestManageExternalStoragePermission();
 
         filePickerLauncher = registerForActivityResult(
                 new ActivityResultContracts.GetContent(),
-                new ActivityResultCallback<Uri>() {
-                    @Override
-                    public void onActivityResult(Uri uri) {
-                        if (uri != null) {
-                            // Read the selected file's path
-                            String filePath = getFilePathFromUri(uri);
-                            String despath=getPath(filePath);
-                            String filename=getname(filePath);
-                            // Set the file path to the TextView
-                            pathview.setText(filePath);
-                            String finalpath=despath+filename;
-                            File dir = new File(finalpath); //以某路径实例化一个File对象
-                            if (!dir.exists()){ //如果不存在
-                                boolean dr = dir.mkdirs(); //创建目录
-                            }
-                            try {
-                                UnzipUtility.main(filePath,finalpath);
-                            } catch (IOException e) {
-                                throw new RuntimeException(e);
-                            }
-                            logview.setText("开始压缩");
-                        } else {
-                            // File selection was canceled
-                            // Handle the cancelation case if needed
+                uri -> {
+                    if (uri != null) {
+                        // Read the selected file's path
+                        String filePath = getFilePathFromUri(uri);
+                        String despath=getPath(filePath);
+                        String filename=getname(filePath);
+                        // Set the file path to the TextView
+                        pathview.setText(filePath);
+                        String finalpath=despath+filename;
+                        File dir = new File(finalpath); //以某路径实例化一个File对象
+                        if (!dir.exists()){ //如果不存在
+                            boolean dr = dir.mkdirs(); //创建目录
+                            System.out.println(dr);
                         }
+                        try {
+                            UnzipUtility.main(filePath,finalpath);
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                        logview.setText("开始压缩");
                     }
                 });
     }
@@ -98,7 +75,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private String getFilePathFromUri(Uri uri) {
-        String filePath = null;
+        String filePath;
         if (DocumentsContract.isDocumentUri(this, uri)) {
             // SAF file picker result
 //            DocumentFile documentFile = DocumentFile.fromSingleUri(this, uri);
@@ -151,7 +128,7 @@ public class MainActivity extends AppCompatActivity {
             try {
                 filePath = cursor.getString(cursor.getColumnIndex(filePathColumn[0]));
                 return filePath;
-            } catch (Exception e) {
+            } catch (Exception ignored) {
             } finally {
                 cursor.close();
             }
@@ -160,14 +137,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void requestManageExternalStoragePermission() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            if (!Environment.isExternalStorageManager()) {
-                Intent intent = new Intent(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION);
-                manageExternalStorageLauncher.launch(intent);
-            } else {
-                // Permission already granted
-                // You can now proceed with your file management operations
-            }
+        // 只在Build.VERSION.SDK_INT >= Build.VERSION_CODES.R，即安卓11以上版本支持
+        if (!Environment.isExternalStorageManager()) {
+            Intent intent = new Intent(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION);
+//            startActivity(intent);
+            manageExternalStorageLauncher.launch(intent);
         }
+
     }
 }
